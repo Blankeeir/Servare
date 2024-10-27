bc1aeec972a9f0d606d092ed93c9063a2353cb4a
Blankeeir <e1300538@u.nus.edu>
2024-10-27 20:27:20 +0800

    update yarn settings

--- apps/contracts/contracts/ServareNFT.sol
+++ apps/contracts/contracts/ServareNFT.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 // Food Product NFT Contract
